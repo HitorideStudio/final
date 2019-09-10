417ed8983d8462c8704dff3951860edce96f2128
ADMIN <ADMIN@LAPTOP-Q817HP2A>
2019-09-10 14:58:50 +0900

    다시 파일 업로드

---
+++ project2_final/src/project_2/bean/GoBean.java
@@ -0,0 +1,38 @@
+package project_2.bean;
+
+import java.util.List;
+
+import javax.servlet.http.HttpSession;
+
+import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import project.list.vo.PlaceList;
+import project.member.vo.memberVO;
+
+
+@Controller
+@RequestMapping("/finder/")
+public class GoBean {
+	
+	//my batis db
+	@Autowired
+	private SqlSessionTemplate sql = null;
+	
+	@RequestMapping("main.do")
+	public String main() {
+		
+		return "/finder/main";
+	}
+	
+	@RequestMapping("info.do")
+	private String info() {
+		
+		return "/finder/info";
+	}
+
+}
