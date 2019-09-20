30257eb213a3983c7d58ef50eef9eeb09fa71310
HitorideStudio <mfboy19@gmail.com>
2019-09-20 18:21:01 +0900

    마커 댓글 작성중- 건훈

--- project2_final/src/project_2/bean/Infoboard.java
+++ project2_final/src/project_2/bean/Infoboard.java
@@ -1,8 +1,12 @@
 package project_2.bean;
 
 import java.io.File;
+import java.util.HashMap;
+import java.util.List;
 
+import javax.security.auth.message.callback.PrivateKeyCallback.Request;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 
 import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -23,13 +27,50 @@ public class Infoboard {
 	private SqlSessionTemplate sql = null;
 	
 	@RequestMapping("writeForm")
-	public ModelAndView writeForm(String place,String placename,String number) {
-		ModelAndView model = new ModelAndView();
-		model.addObject("place",place);
-		model.addObject("placename",placename);
-		model.addObject("number",number);
-		model.setViewName("/infoboard/writeForm");
-		return model;
+	public String writeForm(HttpServletRequest request,HttpSession session,
+			String place,String placename,String number, Model model) {
+		System.out.println(number);
+		//댓글 카운트
+		int count = (Integer)sql.selectOne("infoboard.count",number);
+		System.out.println(count);
+		//해당 마커 댓글 가져오기
+		int row = 10; //페이지에 보여줄 갯수
+		String page = request.getParameter("page");
+		if(page == null) {
+			page="1";
+		}
+		int currentPage = Integer.parseInt(page);
+		int startRow = (currentPage-1) * row +1;    // 각 페이지당 시작하는 번호 ex) row = 10일때 startRow = 1 , 11 , 21 ....
+		int endRow = currentPage * row;    // 각 페이지당 끝나는 번호 ex) row = 10일때 startRow = 10 , 20, 30 ....
+		HashMap pageList = new HashMap();     // sql 문에 보내주기 위해 생성
+ 
+		pageList.put("number", number);
+		pageList.put("startRow",startRow);
+		pageList.put("endRow",endRow);
+		
+		// 댓글 가져오기
+		List revo = sql.selectList("infoboard.replylist",pageList);
+		System.out.println(revo);
+		//페이지 계산
+		int pageCount = count / row + (count % row == 0? 0:1);
+		int startPage = (int)(currentPage/10)*10+1;
+		int pageBlock=10;
+		int endPage = startPage + pageBlock-1;
+		if(endPage > pageCount) endPage = pageCount;
+		
+		System.out.println(pageCount);
+		model.addAttribute("place",place);
+		model.addAttribute("placename",placename);
+		model.addAttribute("number",number);
+		model.addAttribute("revo",revo);
+		model.addAttribute("count",count);
+		model.addAttribute("startPage",startPage);
+		model.addAttribute("endPage",endPage);
+		model.addAttribute("pageCount",pageCount);
+		
+		
+		
+		return "/infoboard/writeForm";
 	}
 	
 	@RequestMapping("writePro.do")
@@ -61,7 +102,7 @@ public class Infoboard {
 		vo.setI_placename(i_placename);
 		
 		sql.insert("infoboard.write",vo);
-		model.addAttribute("i_img",i_img);
+		model.addAttribute("i_placename",i_placename);
 		
 		
 		return "/infoboard/writePro";
